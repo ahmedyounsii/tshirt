f3bfcc718c7f2062f8e8d74563c82508ee71ef9b
YounsiAhmed <hammoua07@gmail.com>
2025-05-25 15:50:33 +0100

    update

--- tshirt/server/main.server.mjs
+++ tshirt/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-U6KNML5A.mjs";import"./chunk-SDQPZGSE.mjs";import"./chunk-4FE4DVQU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-VXRCHIRL.mjs";import"./chunk-SDQPZGSE.mjs";import"./chunk-4FE4DVQU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
